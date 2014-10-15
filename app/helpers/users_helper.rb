3739fe94515d2ad725f8d5a146d41838c8ba6dd0
Muhammad Yossry <mohamedyosry3000@gmail.com>
2014-10-15 09:42:26 +0200

    Sign-up exercises

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, opts = {} )
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}" +
+                   (opts[:size] ? "?s=#{opts[:size].to_s}" : "")
+
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
